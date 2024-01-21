7fca0497957a16d1bad9e6f492c89cde8937e973
moviefan322 <philman202@yahoo.com>
2024-01-21 12:23:18 -0500

    builds out sidebar dropdowns

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,19 +1,101 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./SideMenu.module.css";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import Link from "next/link";
 
 interface SideMenuProps {
   isOpen: boolean;
 }
 
 const SideMenu = ({ isOpen }: SideMenuProps) => {
+  const [showVisitMenu, setShowVisitMenu] = useState(false);
+  const [showEventsMenu, setShowEventsMenu] = useState(false);
+  const [showAboutMenu, setShowAboutMenu] = useState(false);
+  const [showBusinessMenu, setShowBusinessMenu] = useState(false);
+
   return (
     <div className={`${styles.sideMenu} ${isOpen ? `${styles.open}` : ""}`}>
       <ul className={styles.customList}>
-        <li>Visit</li>
-        <li>Events</li>
-        <li>About Us</li>
-        <li>Business Resources</li>
+        <li>
+          <button
+            className="noStyleButt"
+            onClick={() => setShowVisitMenu((prev) => !prev)}
+          >
+            {showVisitMenu ? "-" : "+"} VISIT
+          </button>
+        </li>
+        {showVisitMenu && (
+          <ul className={styles.noStyleLi}>
+            <li>DOWNTOWN BUSINESSES</li>
+            <li>
+              <Link
+                className={styles.noStyleLink}
+                href="https://www.asbury.edu/admissions-aid/"
+              >
+                ASBURY UNIVERSITY
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={styles.noStyleLink}
+                href="https://www.stoneponyonline.com/"
+              >
+                THE STONE PONY
+              </Link>
+            </li>
+            <li>PARKING</li>
+            <li>PHOTO TOUR</li>
+          </ul>
+        )}
+
+        <li>
+          {" "}
+          <button
+            className="noStyleButt"
+            onClick={() => setShowEventsMenu((prev) => !prev)}
+          >
+            {showEventsMenu ? "-" : "+"} EVENTS
+          </button>
+          {showEventsMenu && (
+            <ul className={styles.noStyleLi}>
+              <li>UPCOMING EVENTS</li>
+              <li>RECURRING EVENTS</li>
+              <li>FARMERS MARKET</li>
+            </ul>
+          )}
+        </li>
+        <li>
+          {" "}
+          <button
+            className="noStyleButt"
+            onClick={() => setShowAboutMenu((prev) => !prev)}
+          >
+            {showAboutMenu ? "-" : "+"} ABOUT US
+          </button>
+          {showAboutMenu && (
+            <ul className={styles.noStyleLi}>
+              <li>MISSION & STAFF</li>
+              <li>VOLUNTEER</li>
+              <li>CONTACT US</li>
+            </ul>
+          )}
+        </li>
+        <li>
+          {" "}
+          <button
+            className="noStyleButt"
+            onClick={() => setShowBusinessMenu((prev) => !prev)}
+          >
+            {showBusinessMenu ? "-" : "+"} BUSINESS RESOURCES
+          </button>
+          {showBusinessMenu && (
+            <ul className={styles.noStyleLi}>
+              <li>UPCOMING EVENTS</li>
+              <li>RECURRING EVENTS</li>
+              <li>FARMERS MARKET</li>
+            </ul>
+          )}
+        </li>
         <FaMagnifyingGlass className={styles.searchIcon} />
         <br />
         <button className={`noStyleButt ${styles.donateButt}`}>DONATE</button>
