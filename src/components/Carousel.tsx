13713c3596460c1447222e00252e1cab1c10d6a2
moviefan322 <philman202@yahoo.com>
2024-01-21 16:52:46 -0500

    fixes carousel scroll functionality

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,32 +4,54 @@ import styles from "./Carousel.module.css";
 
 function Carousel() {
   return (
-    <section className={`${styles.carousel} text-white`}>
-      <div className={`${styles.matte}`}></div>
-      <div className="carousel slide">
+    <section id="testimonials" className={`${styles.carousel} text-white`}>
+      <div id="myCarousel" className="carousel slide">
+        <div className={`${styles.matte}`}></div>
         <div className="carousel-inner">
-          {/* <!-- Slide 1 --> */}
-          <div
-            className={`${styles.carouselItem1} active d-flex flex-column justify-content-center align-items-center`}
-          >
-            <h1 className={styles.zindex}>Annual Town Hall</h1>
-            <h1 className={styles.zindex}>Wed Feb 7</h1>
-            <h1 className={styles.zindex}>6:30pm</h1>
-            <button className={`noStyleButt ${styles.carouselButt}`}>RSVP Here</button>
+          {/* Slide 1 */}
+          <div className="carousel-item active">
+            <div
+              className={`${styles.carouselItem1} d-flex flex-column justify-content-center align-items-center`}
+            >
+              <h1 className={styles.zindex}>Annual Town Hall</h1>
+              <h1 className={styles.zindex}>Wed Feb 7</h1>
+              <h1 className={styles.zindex}>6:30pm</h1>
+              <button className={`noStyleButt ${styles.carouselButt}`}>
+                RSVP Here
+              </button>
+            </div>
           </div>
-          {/* <!-- Slide 2 --> */}
+          {/* Slide 2 */}
           <div className="carousel-item">
-            <h1 className={styles.zindex}>My</h1>
+            <div
+              className={`${styles.carouselItem2} d-flex flex-column justify-content-center align-items-center`}
+            >
+              <h1 className={styles.zindex}>Annual Town Hall</h1>
+              <h1 className={styles.zindex}>Wed Feb 7</h1>
+              <h1 className={styles.zindex}>6:30pm</h1>
+              <button className={`noStyleButt ${styles.carouselButt}`}>
+                RSVP Here
+              </button>
+            </div>
           </div>
-          {/* <!-- Slide 3 --> */}
+          {/* Slide 3 */}
           <div className="carousel-item">
-            <h1>Balls</h1>
+            <div
+              className={`${styles.carouselItem3} d-flex flex-column justify-content-center align-items-center`}
+            >
+              <h1 className={styles.zindex}>Annual Town Hall</h1>
+              <h1 className={styles.zindex}>Wed Feb 7</h1>
+              <h1 className={styles.zindex}>6:30pm</h1>
+              <button className={`noStyleButt ${styles.carouselButt}`}>
+                RSVP Here
+              </button>
+            </div>
           </div>
         </div>
         <button
           className="carousel-control-prev"
           type="button"
-          data-bs-target="#testimonialCarousel"
+          data-bs-target="#myCarousel"
           data-bs-slide="prev"
         >
           <FaChevronLeft className={styles.zindex} />
@@ -37,7 +59,7 @@ function Carousel() {
         <button
           className="carousel-control-next"
           type="button"
-          data-bs-target="#testimonialCarousel"
+          data-bs-target="#myCarousel"
           data-bs-slide="next"
         >
           <FaChevronRight className={styles.zindex} />
