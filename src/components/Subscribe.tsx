7cab168a53f841584ac2ee53d2fdbf295f40d474
moviefan322 <philman202@yahoo.com>
2024-01-22 16:21:55 -0500

    fully responsive subscribe section

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -3,14 +3,17 @@ import styles from "./Subscribe.module.css";
 
 const Subscribe = () => {
   return (
-    <div className="container bg-dark d-flex flex-column align-items-center">
+    <div className="bg-dark d-flex flex-column align-items-center">
       <div className={styles.form}>
         <h6>SUBSCRIBE</h6>
         <p>Sign up with your email address to receive news and updates.</p>
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Email Address" />
-        <button className="noStyleButt orangeButt">SIGN UP</button>
+        <div className={styles.formform}>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Email Address" />
+          <button className="noStyleButt orangeButt">SIGN UP</button>
+        </div>
+        <div></div>{" "}
       </div>
     </div>
   );
