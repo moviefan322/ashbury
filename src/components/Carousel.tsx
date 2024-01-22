5403e63b07f740fa804f8355171779b839696d1f
moviefan322 <philman202@yahoo.com>
2024-01-21 23:49:32 -0500

    adds carousel autoslide and Greetgings section

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,11 +5,11 @@ import styles from "./Carousel.module.css";
 function Carousel() {
   return (
     <section id="testimonials" className={`${styles.carousel} text-white`}>
-      <div id="myCarousel" className="carousel slide">
+      <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
         <div className={`${styles.matte}`}></div>
         <div className="carousel-inner">
           {/* Slide 1 */}
-          <div className="carousel-item active">
+          <div className="carousel-item active" data-bs-interval="5000">
             <div
               className={`${styles.carouselItem1} d-flex flex-column justify-content-center align-items-center`}
             >
@@ -22,28 +22,25 @@ function Carousel() {
             </div>
           </div>
           {/* Slide 2 */}
-          <div className="carousel-item">
+          <div className="carousel-item" data-bs-interval="5000">
             <div
               className={`${styles.carouselItem2} d-flex flex-column justify-content-center align-items-center`}
             >
-              <h1 className={styles.zindex}>Annual Town Hall</h1>
-              <h1 className={styles.zindex}>Wed Feb 7</h1>
-              <h1 className={styles.zindex}>6:30pm</h1>
+              <h1 className={styles.zindex}>BOARDWALK</h1>
+              <h1 className={styles.zindex}>GIFT CERTIFICATES</h1>
               <button className={`noStyleButt ${styles.carouselButt}`}>
-                RSVP Here
+                Purchase Here
               </button>
             </div>
           </div>
           {/* Slide 3 */}
-          <div className="carousel-item">
+          <div className="carousel-item" data-bs-interval="5000">
             <div
               className={`${styles.carouselItem3} d-flex flex-column justify-content-center align-items-center`}
             >
-              <h1 className={styles.zindex}>Annual Town Hall</h1>
-              <h1 className={styles.zindex}>Wed Feb 7</h1>
-              <h1 className={styles.zindex}>6:30pm</h1>
+              <h1 className={styles.zindex}>SHOP LOCAL</h1>
               <button className={`noStyleButt ${styles.carouselButt}`}>
-                RSVP Here
+                TOWN DIRECTORY
               </button>
             </div>
           </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Carousel from "@/components/Carousel";
+import Greetings from "@/components/Greetings";
 
 export default function Home() {
   return (
     <>
       <Carousel />
+      <Greetings />
+      <hr className='w-75 mx-auto' />
+      <h1>Poop</h1>
     </>
   );
 }
