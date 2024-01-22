fb9e1d3f59b251b19e22f6e30d3598e68ad2e2d0
moviefan322 <philman202@yahoo.com>
2024-01-22 00:08:06 -0500

    fixes carousel bug

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,69 +1,45 @@
-/* eslint-disable react/no-unescaped-entities */
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
-import styles from "./Carousel.module.css";
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import styles from './Carousel.module.css';
 
-function Carousel() {
+function CustomCarousel() {
   return (
-    <section id="testimonials" className={`${styles.carousel} text-white`}>
-      <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
-        <div className={`${styles.matte}`}></div>
-        <div className="carousel-inner">
-          {/* Slide 1 */}
-          <div className="carousel-item active" data-bs-interval="5000">
-            <div
-              className={`${styles.carouselItem1} d-flex flex-column justify-content-center align-items-center`}
-            >
-              <h1 className={styles.zindex}>Annual Town Hall</h1>
-              <h1 className={styles.zindex}>Wed Feb 7</h1>
-              <h1 className={styles.zindex}>6:30pm</h1>
-              <button className={`noStyleButt ${styles.carouselButt}`}>
-                RSVP Here
-              </button>
-            </div>
+    <section id="testimonials" className="text-white">
+      <Carousel id="myCarousel" slide>
+        <Carousel.Item interval={5000}>
+          <div className={`${styles.carouselItem1} d-flex flex-column justify-content-center align-items-center`}>
+            <div className={`${styles.matte}`}></div>
+            <h1>Annual Town Hall</h1>
+            <h1>Wed Feb 7</h1>
+            <h1>6:30pm</h1>
+            <button className={`noStyleButt ${styles.carouselButt}`}>
+              RSVP Here
+            </button>
           </div>
-          {/* Slide 2 */}
-          <div className="carousel-item" data-bs-interval="5000">
-            <div
-              className={`${styles.carouselItem2} d-flex flex-column justify-content-center align-items-center`}
-            >
-              <h1 className={styles.zindex}>BOARDWALK</h1>
-              <h1 className={styles.zindex}>GIFT CERTIFICATES</h1>
-              <button className={`noStyleButt ${styles.carouselButt}`}>
-                Purchase Here
-              </button>
-            </div>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <div className={`${styles.carouselItem2} d-flex flex-column justify-content-center align-items-center`}>
+            <div className={`${styles.matte}`}></div>
+            <h1>BOARDWALK</h1>
+            <h1>GIFT CERTIFICATES</h1>
+            <button className={`noStyleButt ${styles.carouselButt}`}>
+              Purchase Here
+            </button>
           </div>
-          {/* Slide 3 */}
-          <div className="carousel-item" data-bs-interval="5000">
-            <div
-              className={`${styles.carouselItem3} d-flex flex-column justify-content-center align-items-center`}
-            >
-              <h1 className={styles.zindex}>SHOP LOCAL</h1>
-              <button className={`noStyleButt ${styles.carouselButt}`}>
-                TOWN DIRECTORY
-              </button>
-            </div>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <div className={`${styles.carouselItem3} d-flex flex-column justify-content-center align-items-center`}>
+            <div className={`${styles.matte}`}></div>
+            <h1>SHOP LOCAL</h1>
+            <button className={`noStyleButt ${styles.carouselButt}`}>
+              TOWN DIRECTORY
+            </button>
           </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide="prev"
-        >
-          <FaChevronLeft className={styles.zindex} />
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide="next"
-        >
-          <FaChevronRight className={styles.zindex} />
-        </button>
-      </div>
+        </Carousel.Item>
+      </Carousel>
     </section>
   );
 }
 
-export default Carousel;
+export default CustomCarousel;
