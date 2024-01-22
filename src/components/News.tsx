2ea31a1276e18de92d8e1e28309dec7ba6da3d36
moviefan322 <philman202@yahoo.com>
2024-01-22 11:12:08 -0500

    completes news component

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -5,16 +5,17 @@ import Image from "next/image";
 
 const News = () => {
   return (
-    <div className="container d-flex flex-column align-items-center">
-      <h4 className={`${styles.newsh4} mb-5`}>NEWS AND PROMOTIONS</h4>
-      <div className="d-flex row mx-auto">
-        <div className="col-md-3 d-flex flex-column align-items-center">
+    <div className="container d-flex flex-column align-items-center pb-5">
+      <h4 className={`${styles.newsh4} my-4 fw-bold`}>NEWS AND PROMOTIONS</h4>
+      <div className="row mx-auto mt-3">
+        <div className="col-12 col-md-3 mb-4 d-flex flex-column align-items-center">
           <Image
-            className="mb-3"
+            className="mb-2 align-self-center"
             src="/artisan.jpeg"
-            height={220}
-            width={260}
+            height={250}
+            width={290}
             alt="artisan crafting"
+            layout="responsive"
           />
           <h4 className={styles.newsHead}>
             ASBURY PARK'S CREATIVE COLLECTIVE POP-UP CALENDAR FOR JAN/FEB IS NOW
@@ -24,34 +25,62 @@ const News = () => {
             Vendor Opportunity: Reserve your spot at Asbury Park's Creative
             Collective Pop-Up
           </p>
+          <p className="text-primary align-self-start">Read More →</p>
         </div>
+        <div className="col-12 col-md-3 mb-4 d-flex flex-column align-items-center">
+          <Image
+            className="mb-2 align-self-center"
+            src="/njcoffee.jpeg"
+            height={250}
+            width={290}
+            alt="jersey coffee"
+            layout="responsive"
+          />
+          <h4 className={styles.newsHead}>
+            BOARDWALK BEANS LISTED AMONG NJ.COM'S BEST COFFEE SHOPS IN NJ
+          </h4>
+          <p>Kudos to Boardwalk Beans for making the list!</p>
+          <p className="text-primary align-self-start">Read More →</p>
+        </div>
+        <div className="col-12 col-md-3 mb-4 d-flex flex-column align-items-center">
+          <Image
+            className="mb-2 align-self-center"
+            src="/flower.jpeg"
+            height={250}
+            width={290}
+            layout="responsive"
+            alt="artisan crafting"
+          />
+          <h4 className={styles.newsHead}>
+            SEASIDE GREENERY CELEBRATES ITS GRAND OPENING ON FRI DEC 1ST
+          </h4>
+          <p>Join the celebration at Seaside Greenery's GRAND OPENING!</p>
+          <p className="text-primary align-self-start">Read More →</p>
+        </div>
+        <div className="col-12 col-md-3 mb-4 d-flex flex-column align-items-center">
+          <Image
+            className="mb-2 align-self-center"
+            src="/sword.jpeg"
+            height={250}
+            width={290}
+            layout="responsive"
+            alt="artisan crafting"
+          />
+          <h4 className={styles.newsHead}>
+            ASBURY PARK WELCOMES THE CREATIVE COLLECTIVE
+          </h4>
+          <p>
+            The Creative Collective in Asbury Park will host an exclusive
+            preview of their innovative new space.
+          </p>
+          <p className="text-primary align-self-start">Read More →</p>
+        </div>
+      </div>
+      <div>
+        <button className={`noStyleButt greenButt`}> ALL NEWS & PROMOTIONS</button>
       </div>
     </div>
   );
 };
 
 export default News;
-
-// Asbury Park's Creative Collective Pop-Up Calendar for Jan/Feb is Now Available
-// ASBURY PARK'S CREATIVE COLLECTIVE POP-UP CALENDAR FOR JAN/FEB IS NOW AVAILABLE
-// Vendor Opportunity: Reserve your spot at Asbury Park's Creative Collective Pop-Up
-
-// Read More →
-// Jan 2, 2024
-
-// Boardwalk Beans Listed Among NJ.com's Best Coffee Shops in NJ
-// BOARDWALK BEANS LISTED AMONG NJ.COM'S BEST COFFEE SHOPS IN NJ
-// Kudos to Boardwalk Beans for making the list!
-
-// Read More →
-// Dec 5, 2023
-
-// Seaside Greenery Celebrates Its Grand Opening on Fri Dec 1st
-// SEASIDE GREENERY CELEBRATES ITS GRAND OPENING ON FRI DEC 1ST
-// Join the celebration at Seaside Greenery's GRAND OPENING!
-
-// Read More →
-// Nov 28, 2023
-
-// ASBURY PARK WELCOMES THE CREATIVE COLLECTIVE
-// The Creative Collective in Asbury Park will host an exclusive preview of their innovative new space at 123 Ocean Avenue on Saturday from 10am to 7pm. The Creative Collective offers a unique collaborative retail environment to support local artisans and entrepreneurs. The community is invited to discover, shop, and engage with local vendors while learning about their unique crafts and the vision behind The Creative Collective.
